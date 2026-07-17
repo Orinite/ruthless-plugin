cb05817227a06c8f4c1864dfdef4d70b1383ac47
Trentin Thomas <trentinthomas94@gmail.com>
2026-07-17 08:09:53 -0500

    Modified the default to be 2 instead of 1 for ID

--- src/main/java/com/ruthless/utils/Constants.java
+++ src/main/java/com/ruthless/utils/Constants.java
@@ -3,6 +3,5 @@ package com.ruthless.utils;
 public class Constants {
     private Constants(){}
 
-    public static final String RUTHLESS_DISCORD_GUILD_ID = "218474341376065536";
-    public static final Integer RUTHLESS_CLAN_ID = 1;
+    public static final Integer RUTHLESS_CLAN_ID = 2;
 }
